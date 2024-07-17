d61efc0c0529be7a34732115dcce8e6568bad59a
yeoun0570 <yeoun0570@gmail.com>
2024-07-17 12:40:38 +0900

    240717 Java에서 제공하는 함수형 인터페이스

--- src/stream_lambda/ex/LambdaEx01.java
+++ src/stream_lambda/ex/LambdaEx01.java
@@ -1,2 +1,26 @@
-package stream_lambda.ex;public class LambdaEx01 {
+package stream_lambda.ex;
+
+//함수형 인터페이스란? 함수를 변수처럼 선언해서 사용하는 개념
+//코드를 간결하게 작성할 수 있다.
+@FunctionalInterface
+interface Func1{
+  int max(int n1, int n2);
+}
+public class LambdaEx01 {
+
+  public static void main(String[] args) {
+    //익명 함수
+        /*System.out.println(new Func1(){
+
+          public int max(int n1, int n2){
+            return n1 > n2 ? n1 : n2;
+          }
+
+        }.max(5,7));*/
+
+        // 순수 함수 : 람다식으로 생성된 함수로써 함수형 인터페이스로만 선언이 가능하다.
+        Func1 func1 = (int a, int b) -> a > b ? a : b; //위의 주석과 동일
+        System.out.println(func1.max(3,5));
+
+  }
 }
--- src/stream_lambda/funcInterface/FunctionInterfaceEx.java
+++ src/stream_lambda/funcInterface/FunctionInterfaceEx.java
@@ -1,2 +1,52 @@
-package stream_lambda.funcInterface;public class FunctionInterfaceEx {
+package stream_lambda.funcInterface;
+
+/* Supplier<T> 형태
+@FunctionalInterface
+public interface Supplier<T> {
+  T get();
+}*/
+
+/* Consumer<T> 형태
+@FunctionalInterface
+public interface Consumer<T> {
+  void accept(T t);
+
+  default Consumer<T> andThen(Consumer<? super T> after){
+    Object.requiredonNull(after);
+    return (T t) -> {accept(t); after.accept(t);
+  };
+
+}*/
+
+//Function<T,R> 과 Predicate<T>는 ctrl + enter로 직접 들어가서 확인
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionInterfaceEx {
+
+  public static void main(String[] args) {
+
+    Supplier<String> supplier = () -> "Hello Supplier!";
+    System.out.println(supplier.get());
+
+    System.out.println("=============================");
+
+    Consumer<String> consumer = (str -> System.out.println(str.split(" ")[0]));
+    consumer.andThen(System.out::println).accept("Hello Consumer!");
+
+    System.out.println("=============================");
+
+    Function<String, Integer> function = str -> str.length();
+    //Function<String, Integer> function = String::length; //위와 동일
+    System.out.println(function.apply("Hello Function Interface!"));
+
+    System.out.println("=============================");
+
+    Predicate<String> predicate = (str -> str.equals("Hello Predicate!"));
+    System.out.println(predicate.test("Predicate"));
+
+  }
 }
